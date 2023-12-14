24036efb573e4989f7c2c492bcbcc3eccaca2708
Shult <sylvain@PersoSylvain>
2023-12-14 18:08:14 +0100

    fusion projet Antoine

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+interface Student {
+  _id: string;
+  firstName: string;
+  lastName: string;
+  className: string;
+}
+
+const App: React.FC = () => {
+  const [students, setStudents] = useState<Student[]>([]);
+  const [newStudent, setNewStudent] = useState({ firstName: '', lastName: '', className: '' });
+
+  useEffect(() => {
+    fetch('/api/students')
+      .then(response => response.json())
+      .then(data => setStudents(data));
+  }, []);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setNewStudent({ ...newStudent, [name]: value });
+  };
+
+  const handleAddStudent = () => {
+    fetch('/api/students', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newStudent),
+    })
+      .then(response => response.json())
+      .then(data => setStudents([...students, data]));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        Hello World !
-      </header>
+    <div>
+      <h1>School Management System</h1>
+      <div>
+        <h2>Add Student</h2>
+        <input type="text" name="firstName" placeholder="First Name" onChange={handleInputChange} />
+        <input type="text" name="lastName" placeholder="Last Name" onChange={handleInputChange} />
+        <input type="text" name="className" placeholder="Class" onChange={handleInputChange} />
+        <button onClick={handleAddStudent}>Add Student</button>
+      </div>
+      <div>
+        <h2>All Students</h2>
+        <ul>
+          {students.map(student => (
+            <li key={student._id}>
+              {student.firstName} {student.lastName} - {student.className}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
