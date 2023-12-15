d31d3f8e301d98c110a1e5230c276f8e06c9a285
Shult <sylvain@PersoSylvain>
2023-12-15 17:38:28 +0100

    api2

--- api.js
+++ api.js
@@ -9,12 +9,11 @@ const db = mysql.createConnection({
   host: 'db', // Nom du service dans docker-compose
   user: 'root', // Modifiez selon vos paramètres
   password: 'password', // Modifiez selon vos paramètres
-  date: Date.now,
   database: 'cours' // Modifiez selon vos paramètres
 });
 
 // Connexion à la base de données
-db.connect((err) => {
+db.connect(err => {
   if (err) {
     console.error('Erreur de connexion à la base de données:', err);
     return;
@@ -22,11 +21,15 @@ db.connect((err) => {
   console.log('Connecté à la base de données');
 });
 
+// Middleware pour parser le JSON
+app.use(express.json());
+
 // Route pour tester la connexion
 app.get('/', (req, res) => {
   res.send('API fonctionnelle');
 });
 
+// Route pour obtenir toutes les personnes
 app.get('/personnes', (req, res) => {
   db.query('SELECT * FROM personne', (err, results) => {
     if (err) {
@@ -36,25 +39,20 @@ app.get('/personnes', (req, res) => {
   });
 });
 
-app.use(express.json());
-
+// Route pour ajouter une personne
 app.post('/personnes', (req, res) => {
-  const { nom, prenom, class, date } = req.body;
-  db.query('INSERT INTO personne (nom, prenom, class, date) VALUES (?, ?, ?, ?)', 
-    [nom, prenom, class, date], 
+  const { nom, prenom, classe } = req.body; // 'class' est un mot-clé réservé en JS, utilisons 'classe'
+  db.query('INSERT INTO personne (nom, prenom, classe) VALUES (?, ?, ?)', 
+    [nom, prenom, classe], 
     (err, result) => {
       if (err) {
         return res.status(500).json({ error: 'Erreur lors de l\'ajout de la personne' });
       }
-      res.status(201).json({ id: result.insertId, nom, prenom, class, date });
+      res.status(201).json({ id: result.insertId, nom, prenom, classe });
   });
 });
-
-
-
 
 // Démarrage du serveur
 app.listen(port, () => {
   console.log(`Serveur démarré sur http://localhost:${port}`);
 });
-
